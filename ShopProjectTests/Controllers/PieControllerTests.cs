629132f520437cd635841fcd07c4e3456b253d7a
Karim shady <kshady960@gmail.com>
2025-09-23 16:29:18 +0300

    Add test for the controllers

---
+++ ShopProjectTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopProject.Controllers;
+using ShopProject.ViewModels;
+using ShopProjectTests.Mocks;
+
+namespace ShopProjectTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnAllPies()
+        {
+            //arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object,mockCategoryRepository.Object);
+
+            //act
+            var result = pieController.List("");
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsType<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
